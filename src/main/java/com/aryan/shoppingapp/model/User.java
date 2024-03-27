5dad82a3cfa0962e08aee40c8fc53f8cc1e06e82
Aryan Patil <aryanitinpatil@gmail.com>
2024-03-27 22:39:26 +0530

    Create User.java

---
+++ src/main/java/com/aryan/shoppingapp/model/User.java
@@ -0,0 +1,26 @@
+package com.aryan.shoppingapp.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+		
+	// Additional user information
+	
+    @OneToMany
+	List<Order> orders;
+}
